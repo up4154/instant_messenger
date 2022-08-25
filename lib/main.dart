2c72b259db99c3369947cc4ed166fd573aa5a980
UTSAV <utsavporwal01@gmail.com>
2022-08-25 23:39:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Instant Messenger',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
